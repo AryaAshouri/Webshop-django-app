5e23b1ca9406e49ea15d68e58a3f6d1d991f3591
Arya <arya.tesla1856@gmail.com>
2023-08-28 03:20:55 -0700

    changed site icon and navbar(toolbar)

--- Branch/static/js/index.js
+++ Branch/static/js/index.js
@@ -7,30 +7,12 @@ function load_search(){
     document.getElementById("location-text").style.fontSize = "0px"
     document.getElementById("location-text").style.marginLeft = "0px"
 
-    document.getElementById("search-bar").style.width = "240px"
+    document.getElementById("search-bar").style.width = "800px"
     document.getElementById("search-bar").style.height = "60px"
     document.getElementById("search-bar").style.paddingLeft = "30px"
     document.getElementById("search-bar").style.paddingRight = "30px"
     document.getElementById("search-bar").style.fontSize = "30px"
     document.getElementById("search-bar").style.marginLeft = "10px"
-  }
-  else {
-    if (document.getElementById("search-bar").style.width == "0px"){
-      document.getElementById("search-bar").style.width = "240px"
-      document.getElementById("search-bar").style.height = "60px"
-      document.getElementById("search-bar").style.paddingLeft = "30px"
-      document.getElementById("search-bar").style.paddingRight = "30px"
-      document.getElementById("search-bar").style.fontSize = "30px"
-      document.getElementById("search-bar").style.marginLeft = "10px"
-    }
-    else if (document.getElementById("search-bar").style.width != "0px"){
-      document.getElementById("search-bar").style.width = "0px"
-      document.getElementById("search-bar").style.height = "0px"
-      document.getElementById("search-bar").style.paddingLeft = "0px"
-      document.getElementById("search-bar").style.paddingRight = "0px"
-      document.getElementById("search-bar").style.fontSize = "0px"
-      document.getElementById("search-bar").style.marginLeft = "0px"
-    }
   }
 }
 function load_location(){
@@ -42,30 +24,12 @@ function load_location(){
     document.getElementById("search-bar").style.fontSize = "0px"
     document.getElementById("search-bar").style.marginLeft = "0px"
 
-    document.getElementById("location-text").style.width = "240px"
+    document.getElementById("location-text").style.width = "800px"
     document.getElementById("location-text").style.height = "56px"
     document.getElementById("location-text").style.paddingLeft = "30px"
     document.getElementById("location-text").style.paddingRight = "30px"
     document.getElementById("location-text").style.fontSize = "30px"
     document.getElementById("location-text").style.marginLeft = "10px"
   }
-  else {
-    if (document.getElementById("location-text").style.width == "0px"){
-      document.getElementById("location-text").style.width = "240px"
-      document.getElementById("location-text").style.height = "56px"
-      document.getElementById("location-text").style.paddingLeft = "30px"
-      document.getElementById("location-text").style.paddingRight = "30px"
-      document.getElementById("location-text").style.fontSize = "30px"
-      document.getElementById("location-text").style.marginLeft = "10px"
-    }
-    else if (document.getElementById("location-text").style.width != "0px"){
-      document.getElementById("location-text").style.width = "0px"
-      document.getElementById("location-text").style.height = "0px"
-      document.getElementById("location-text").style.paddingLeft = "0px"
-      document.getElementById("location-text").style.paddingRight = "0px"
-      document.getElementById("location-text").style.fontSize = "0px"
-      document.getElementById("location-text").style.marginLeft = "0px"
-    }
-  }
 }
-AOS.init();
+AOS.init()
