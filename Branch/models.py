03fc82d6440002913ecca87de9d5663bf240dafb
Arya <arya.tesla1856@gmail.com>
2023-08-31 00:35:41 -0700

    finished picture uploading

--- Branch/models.py
+++ Branch/models.py
@@ -25,9 +25,10 @@ class Product(models.Model):
 	name = models.CharField(max_length = 20, null = True)
 	price = models.CharField(max_length = 90, null = True)
 	category = models.CharField(max_length = 50, null = True)
-	status = models.CharField(max_length = 1, choices = status)
+	image = models.ImageField(null=True, upload_to = "Images")
 	publish = models.DateTimeField(default = timezone.now)
 	created = models.DateTimeField(auto_now_add = True)
 	created = models.DateTimeField(auto_now = True)
+	status = models.CharField(max_length = 1, choices = status)
 	def __str__(self):
 		return self.name
--- Branch/views.py
+++ Branch/views.py
@@ -8,16 +8,20 @@ from .models import *
 app_name = "Branch"
 
 def home(request):
-	context = {"Categories" : Categorie.objects.filter(status = "p").order_by("-publish")}
+	context = {"Categories" : Categorie.objects.filter(status = "p").order_by("-publish"),
+	"Products" : Product.objects.filter(status = "p").order_by("-publish"),
+	}
 	return render(request, "index.html", context)
 
 def category(request):
 	return render(request, "category.html")
 
 def product(request):
-	name = request.POST.get("product")
-	price = request.POST.get("price")
-	category = request.POST.get("category")
-	Product.objects.create(name=name, price=price, category=category, status="p")
-	context = {}
-	return render(request, "product.html", context=context)
+	if request.method == "POST":
+		name = request.POST.get("product")
+		price = request.POST.get("price")
+		category = request.POST.get("category")
+		image = request.FILES.get("image")
+		Product.objects.create(name=name, price=price, category=category, image=image, status="p")
+
+	return render(request, "product.html")
